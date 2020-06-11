64998a3131d42902a445865a2149492951854049
clonemasterUwU <huskar.the.scared.warrior@gmail.com>
2020-06-11 22:03:09 +0700

    route for mobile privacy policy

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import env from 'dotenv'
 env.config({ path:"./debug.env" })
 
 import mobileRoute from './routes/mobile.js'
+import path from 'path'
 const app = express()
+const __dirname=path.resolve()
 
 
 app.use((req, res, next) => {
@@ -12,7 +14,9 @@ app.use((req, res, next) => {
 })
 
 // app.use(express.json())
-
+app.get('/policy',(req,res)=>{
+  res.sendFile(path.join(__dirname,'./api/assets/policy.html'))
+})
 
 app.use('/api/mobile',mobileRoute)
 app.listen(3000,()=>console.log("Server is listening"))
